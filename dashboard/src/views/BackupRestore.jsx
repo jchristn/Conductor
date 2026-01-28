bd6f1f3cb508fe35155ccda66582523f86249862
Joel Christner <joel.christner@gmail.com>
2026-01-28 14:02:25 -0800

    Restore button styling

--- dashboard/src/views/BackupRestore.jsx
+++ dashboard/src/views/BackupRestore.jsx
@@ -161,7 +161,7 @@ function BackupRestore() {
             <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
-            <span>The backup file contains sensitive data including API keys and hashed passwords. Store it securely.</span>
+            <span>The backup file contains sensitive data including API keys and passwords. Store it securely.</span>
           </div>
           <button onClick={handleBackup} disabled={loading} className="btn-primary">
             {loading ? 'Creating Backup...' : 'Download Backup'}
@@ -173,13 +173,26 @@ function BackupRestore() {
           <h2>Restore from Backup</h2>
 
           <div className="form-group">
-            <label htmlFor="backupFile">Select Backup File</label>
-            <input
-              type="file"
-              id="backupFile"
-              accept=".json"
-              onChange={handleFileSelect}
-            />
+            <label>Select Backup File</label>
+            <div className="file-input-wrapper">
+              <input
+                type="file"
+                id="backupFile"
+                accept=".json"
+                onChange={handleFileSelect}
+                className="file-input-hidden"
+              />
+              <button
+                type="button"
+                className="btn-secondary"
+                onClick={() => document.getElementById('backupFile').click()}
+              >
+                Choose File
+              </button>
+              <span className="file-input-name">
+                {backupData ? 'File loaded' : 'No file chosen'}
+              </span>
+            </div>
           </div>
 
           {/* Validation Results */}
